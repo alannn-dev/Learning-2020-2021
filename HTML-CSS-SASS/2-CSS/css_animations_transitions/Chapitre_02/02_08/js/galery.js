0f09c79a27a625a8c4571527c5b8cb2fe1b8db7d
Alan <81377366+heybeec@users.noreply.github.com>
2022-05-28 13:25:53 +0200

    Delete HTML-CSS-SASS/2-CSS directory

--- HTML-CSS-SASS/2-CSS/css_animations_transitions/Chapitre_02/02_08/js/galery.js
+++
@@ -1,5 +0,0 @@
-//toggle sidebar when clicking navbar search button
-jQuery('#navbarSearchButton').click(function(){
-  jQuery(this).toggleClass('active');//set button in active state when sidebar is displayed
-  jQuery('#mainContentRow').toggleClass('sbHidden sbDisplay');//toggle sidebar on and off
-});//end click
